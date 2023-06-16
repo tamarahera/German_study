51bc499cd1444119ff8d54ac529b66b65cf45e01
tamara_hera <tamara.herashchenko@gmail.com>
2023-06-16 09:24:26 +0200

    add tabs script

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 import hamburger from "./modules/hamburger";
-
+import accordion from "./modules/accordion";
+import modal from "./modules/modal";
+import tabs from "./modules/tabs";
 import switcher from "./modules/switcher";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     hamburger('[data-btn="hamburger-open"]', '[data-btn="hamburger-close"]', '.header__nav', '.header .logo .logo__text', 'header__nav--active', 'logo__text--active', 'header__overlay');
-
+    accordion('.question__btn', 'question__btn--active', 'question__descr--active');
+    modal();
+    tabs('.level__tabs', '.level__tab-btn', '.level__content-text', 'level__tab-btn--active', 'level__content-text--active');
+    switcher();
 });
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,39 @@
+const tabs = (parentSelector, tabSelector, contentSelector, activeClassTab, activeClassContent) => {
+    const parent = document.querySelector(parentSelector),
+        tabs = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+    function hideContent() {
+        content.forEach(item => {
+            item.classList.remove(activeClassContent)
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(activeClassTab);
+        });
+    }
+
+    function showContent(i = 0) {
+        content[i].classList.add(activeClassContent);
+        tabs[i].classList.add(activeClassTab);
+    }
+
+    hideContent();
+    showContent();
+
+    parent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if ((target.classList.contains(tabSelector.replace(/\./, '')) || target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) && (!target.classList.contains(activeClassTab) && target)) {
+            tabs.forEach((item, i) => {
+                if (item === target || item === target.parentNode) { 
+                    // parentNode для того, щоб дочірні ел посилались на головний таргет
+                    hideContent();
+                    showContent(i);
+                }
+            });
+        };
+    });
+};
+
+export default tabs;
